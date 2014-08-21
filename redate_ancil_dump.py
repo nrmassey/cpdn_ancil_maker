64233f4a53215af25352bb1ddf7c8a8c9a51ac1a
Neil Massey <massey@atm.ox.ac.uk>
2014-08-21 17:21:51 +0100

    Allow changing calendar type for dump files

--- redate_ancil_dump.py
+++ redate_ancil_dump.py
@@ -15,14 +15,13 @@ import array
 
 #############################################################################
 
-def redate_ancil_or_dump(infile, outfile, year):
+def redate_ancil_or_dump(infile, outfile, year, calendar):
 	# read the file as a binary file
 	fh = open(infile, 'rb')
 	fix_hdr = read_fixed_header(fh)
 	pp_hdrs = read_pp_headers(fh, fix_hdr)
 	# redate fix_hdr
 	fix_hdr[20] = year
-
 	# current_year
 	c_yr = year
 	c_pos = 0
@@ -37,11 +36,23 @@ def redate_ancil_or_dump(infile, outfile, year):
 			added_year = False
 		pp_hdrs[0+c_pos] = c_yr
 		pp_hdrs[6+c_pos] = c_yr
-		c_pos += fix_hdr[150]
+		# change the calendar if necessary - only applicable to start dumps
+		if calendar == "360":
+			pp_hdrs[12+c_pos] = 2
+		elif calendar == "365":
+			pp_hdrs[12+c_pos] = 1
+
+		c_pos += fix_hdr[150]		
 
 	# get the last time
 	c_pos -= fix_hdr[150]
+
 	fix_hdr[27] = pp_hdrs[0+c_pos]
+	# change the calendar if necessary - only applicable to start dumps
+	if calendar == "360":
+		fix_hdr[7] = 2
+	elif calendar == "365":
+		fix_hdr[7] = 1
 	
 	# read all the data in
 	fh.seek(0)
@@ -50,8 +61,9 @@ def redate_ancil_or_dump(infile, outfile, year):
 	# write the new header into the buffer just read in
 	A = array.array('i')
 	A.fromstring(all_data)
-	A[20] = fix_hdr[20]
-	A[27] = fix_hdr[27]
+	A[0:149] = fix_hdr[0:149]
+#	A[27] = fix_hdr[27]
+#	A[7]  = fix_hdr[7]
 	A[fix_hdr[149]-1:fix_hdr[149]+len(pp_hdrs)-1] = pp_hdrs
 	out_data = A.tostring()
 	# write out the file
@@ -62,7 +74,8 @@ def redate_ancil_or_dump(infile, outfile, year):
 #############################################################################
 
 if __name__ == "__main__":
-	opts, args = getopt.getopt(sys.argv[1:], 'i:o:y:', ['input==', 'output==', 'year=='])
+	opts, args = getopt.getopt(sys.argv[1:], 'i:o:y:c:', ['input==', 'output==', 'year==', 'calendar=='])
+	calendar = "-1"
 	for opt, val in opts:
 		if opt in ['--input', '-i']:
 			infile = val
@@ -70,10 +83,12 @@ if __name__ == "__main__":
 			outfile = val
 		if opt in ['--year', '-y']:
 			date = val
+		if opt in ['--calendar', '-c']:
+			calendar = val
 	try:
 		year = int(date)
 	except:
 		print "Year in format yyyy"
 		sys.exit(0)
 			
-	redate_ancil_or_dump(infile, outfile, year)
+	redate_ancil_or_dump(infile, outfile, year, calendar)
